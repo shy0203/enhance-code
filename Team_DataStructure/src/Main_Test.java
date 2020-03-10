c78623994ee596610076cfb6acc054b629eb2eba
dahyun91 <kimdhdh91@naver.com>
2020-03-10 10:15:27 +0900

    패키지에 있는 클래스 리스트 불러오는 기능 추가

--- Team_DataStructure/src/Main_Test.java
+++ Team_DataStructure/src/Main_Test.java
@@ -1,34 +1,46 @@
-import java.util.LinkedList;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main_Test {
-	
-	static int a;
-	
-	public static void main(String[] args) {	
-		// 최종 실행문 결과출력!!
-		
-		@SuppressWarnings("resource")
-		Scanner research_num = new Scanner(System.in);
-		System.out.print("실험번호 입력 : ");	
-		a = research_num.nextInt();
-		
-		System.out.println("--------------------------------------------------------------------");
-		List_Manual(a);
-		System.out.println("--------------------------------------------------------------------");
-		
-		Remote_Control.Experiment_select(a);
-	}
-	
 
-	public static void List_Manual(int a){
-		// 실험 번호별 설명문
+	static int a;
+
+	
+	public static void main(String[] args) throws ClassNotFoundException,
+			IOException {
+		SearchPackage sp = new SearchPackage();
+		Class[] test = sp.getClasses("Lists");
+
+		for (int i = 0; i < test.length; i++)
+		{			
+			System.out.println(test[i]);
+		}
 		
+
+		// 최종 실행문 결과출력!!
+
+		 @SuppressWarnings("resource")
+		 Scanner research_num = new Scanner(System.in);
+		 System.out.print("실험번호 입력 : ");
+		 a = research_num.nextInt();
+		
+		 System.out
+		 .println("--------------------------------------------------------------------");
+		 List_Manual(a);
+		 System.out
+		 .println("--------------------------------------------------------------------");
+		
+		 Remote_Control.Experiment_select(a);
+	}
+
+	public static void List_Manual(int a) {
+		// 실험 번호별 설명문
+
 		switch (a) {
 		case 0:
 			System.out.println("LinkedList<Integer> (LinkedList.class)");
 			break;
-		
+
 		case 1:
 			System.out.println("CopyList<Integer> (Vector 참고 및 배열 추가)");
 			break;
@@ -38,33 +50,35 @@ public class Main_Test {
 			break;
 
 		case 3:
-			System.out.println("CopyList3<Integer> (Entry의 element를 ArrayList로 변경, ArrayList 공간 확장)");
+			System.out
+					.println("CopyList3<Integer> (Entry의 element를 ArrayList로 변경, ArrayList 공간 확장)");
 			break;
 
 		case 4:
-			System.out.println("CopyList4<Integer> (Entry의 element를 ArrayList로 변경, ArrayList 객체 생성)");
+			System.out
+					.println("CopyList4<Integer> (Entry의 element를 ArrayList로 변경, ArrayList 객체 생성)");
 			break;
 
 		case 5:
 			System.out.println("CopyList5<Integer> (특정 기준을 고정으로 잡고 탐색하는 구조)");
 			break;
-			
+
 		case 6:
 			System.out.println("CopyList6<Integer> (특정 기준을 자동으로 잡고 탐색하는 구조)");
 			break;
-			
+
 		case 7:
 			System.out.println("CopyList7<Integer> (DoublyCircularLinkedList)");
 			break;
-		
+
 		case 8:
 			System.out.println("CopyList8<Integer> (DoublyLinkedList)");
 			break;
-			
+
 		case 9:
 			System.out.println("CopyList9<Integer> (SinglyCircularLinkedList)");
 			break;
-		
+
 		case 10:
 			System.out.println("CopyList10<Integer> (SinglyLinkedList)");
 			break;
